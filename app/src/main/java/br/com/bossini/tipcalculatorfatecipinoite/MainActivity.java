e3872095e02682bda49f366bd846b4c559a11476
Lucas Teles <lucas.teles@univesp.br>
2019-03-01 18:13:08 -0300

    upload changes

--- app/src/main/java/br/com/bossini/tipcalculatorfatecipinoite/MainActivity.java
+++ app/src/main/java/br/com/bossini/tipcalculatorfatecipinoite/MainActivity.java
@@ -1,10 +1,10 @@
 package br.com.bossini.tipcalculatorfatecipinoite;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -19,26 +19,27 @@ public class MainActivity extends AppCompatActivity {
     private double billAmount = 0;
     private double percent = 0.15;
 
-    private EditText amountEditText;
-    private TextView amountTextView;
-    private TextView percentTextView;
-    private SeekBar percentSeekBar;
-    private TextView tipTextView;
-    private TextView totalTextView;
+    private TextView percentEthanolTextView;
+    private TextView percentGasTextView;
+    private SeekBar percentEthanolSeekBar;
+    private SeekBar percentGasSeekBar;
+    private TextInputLayout tipTextView;
+    private ImageView ethanolImageView;
+    private ImageView gasImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        amountEditText = findViewById(R.id.amountEditText);
-        amountTextView = findViewById(R.id.amountTextView);
-        percentTextView = findViewById(R.id.percentTextView);
-        percentSeekBar = findViewById(R.id.percentSeekBar);
+        percentEthanolTextView = findViewById(R.id.percentEthanolTextView);
+        percentEthanolSeekBar = findViewById(R.id.percentEthanolSeekBar);
+        percentGasTextView = findViewById(R.id.percentGasTextView);
+        percentGasSeekBar = findViewById(R.id.percentGasSeekBar);
         tipTextView = findViewById(R.id.tipTextView);
-        totalTextView = findViewById(R.id.totalTextView);
-        amountEditText.
-                addTextChangedListener(textChangedWatcher);
-        percentSeekBar.
+
+        percentEthanolSeekBar.
+                setOnSeekBarChangeListener(seekBarChangeListener);
+        percentGasSeekBar.
                 setOnSeekBarChangeListener(seekBarChangeListener);
     }
 
@@ -47,11 +48,11 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                     percent = progress / 100.;
-                    percentTextView.setText(percentFormat.format(percent));
+                    percentEthanolTextView.setText(percentFormat.format(percent));
                     double tip = percent * billAmount;
                     double billTotal = tip + billAmount;
-                    tipTextView.setText(currencyFormat.format(tip));
-                    totalTextView.setText(currencyFormat.format(billTotal));
+//                    tipTextView.setText(currencyFormat.format(tip));
+//                    totalTextView.setText(currencyFormat.format(billTotal));
                 }
 
                 @Override
@@ -61,38 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            };
-
-
-    private TextWatcher textChangedWatcher =
-            new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    try{
-                        billAmount = Double.parseDouble(s.toString()) / 100;
-                        double tip = percent * billAmount;
-                        double billTotal = billAmount + tip;
-                        tipTextView.setText(currencyFormat.format(tip));
-                        totalTextView.setText(currencyFormat.format(billTotal));
-                        amountTextView.setText(currencyFormat.format(billAmount));
-                    }
-                    catch (NumberFormatException nfe){
-                        amountTextView.setText(currencyFormat.format(0));
-                        tipTextView.setText(currencyFormat.format(0));
-                        totalTextView.setText(currencyFormat.format(0));
-                    }
-
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
 
                 }
             };
